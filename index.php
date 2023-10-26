6a670ac1472a521754615d53ecf0e8585c1184c0
LeonardoFlammini <leonardo98flammini@gmail.com>
2023-10-26 15:49:30 +0200

    [add]scaffolding

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$name= 'Leonardo';
+$array= [
+  'mario',
+  'giuseppe',
+  'bruno'
+];
+
+var_dump($array);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Primo php</title>
+</head>
+<body>
+  <h1><?php echo $name?></h1>
+</body>
+</html>
